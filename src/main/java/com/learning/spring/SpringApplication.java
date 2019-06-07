b73542ad774e619bc6bc700f3fa7ac25a063b28b
fanyuwen <18762677070@163.com>
2019-06-07 11:01:00 +0800

    add the spring init class to project

---
+++ src/main/java/com/learning/spring/SpringApplication.java
@@ -0,0 +1,9 @@
+package com.learning.spring;
+
+/**
+ * @author fanyuwen
+ * Spring总的启动类(采用java bean创建的方式)
+ */
+public class SpringApplication {
+
+}
---
+++ src/main/java/com/learning/spring/SpringConfigure.java
@@ -0,0 +1,13 @@
+package com.learning.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Spring的总配置类
+ */
+@Configuration
+@ComponentScan("com.learning.spring")
+public class SpringConfigure {
+    
+}

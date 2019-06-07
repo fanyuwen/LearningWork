b86925df8fe3c8baf5299f2af6d986770de0b2c3
fanyuwen <18762677070@163.com>
2019-06-07 20:09:05 +0800

    1.添加内部类的基础知识 2.添加mybatis的属性配置文件

---
+++ src/main/java/com/learning/base/innerclass/InnerClass.java
@@ -0,0 +1,55 @@
+package com.learning.base.innerclass;
+
+/**
+ * @author fanyuwen
+ * java基础内部类
+ */
+public class InnerClass {
+
+    private String name;
+
+    /**
+     * 外围类对象也能访问成员内部类对象的私有属性
+     * @param inner
+     */
+    void accessInnerClass(DynamicInner inner){
+        System.out.println(inner.innerName);
+    }
+
+    public static void main(String[] args) {
+        //创建成员内部类的方式,必须先要有外围类的对象
+        InnerClass innerClass = new InnerClass();
+        innerClass.new DynamicInner();
+        //
+    }
+
+    /**
+     * 成员内部类,自动有一个对外围类的引用
+     * 不能创建静态属性
+     */
+    class DynamicInner {
+//        private final InnerClass InnerClass.this; 编译器自动生成,所以能够访问外围类的成员(属性,方法)
+        private String innerName;
+        /**
+         * 可以直接访问外围对象的私有属性和方法
+         */
+        void accessPrivateEnclosing(){
+            System.out.println(name);
+        }
+
+        /**
+         * 即使是参数也是一样的
+         * @param innerClass param
+         */
+        void accessPrivate(InnerClass innerClass){
+            System.out.println(innerClass.name);
+        }
+    }
+
+    /**
+     * 静态内部类,没有
+     */
+    static class StaticInner{
+        
+    }
+}

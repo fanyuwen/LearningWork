389383d1a82246abee19e36874563c7602f5e526
fanyuwen <18762677070@163.com>
2019-06-09 10:45:49 +0800

    修改resource配置

--- spring/src/main/java/com/learning/SpringConfigure.java
+++ spring/src/main/java/com/learning/SpringConfigure.java
@@ -1,10 +1,13 @@
 package com.learning;
 
 import com.learning.resource.ResourceTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.*;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
 import java.util.Arrays;
 
@@ -16,7 +19,8 @@ public class SpringConfigure {
 
     //基于java bean的方式将指定路径上的文件加载为 org.springframework.core.io.Resource
     //Resource指代一个路径资源,任何类型的资源,Spring都可以将它加载成为Resource
-    public ResourceTest createResource() throws MalformedURLException {
+    @Bean
+    public ResourceTest createResource() throws MalformedURLException, FileNotFoundException {
         ResourceTest resourceTest = new ResourceTest();
         Resource[] resources = new Resource[0];
         String path = "";
@@ -33,7 +37,8 @@ public class SpringConfigure {
         resources = Arrays.copyOf(resources, resources.length + 1);
         resources[resources.length - 1] = fileUrlResource;
         //基于文件流
-        InputStreamResource inputStreamResource = new InputStreamResource(null);
+        FileInputStream fileInputStream = new FileInputStream(path);
+        InputStreamResource inputStreamResource = new InputStreamResource(fileInputStream);
         resources = Arrays.copyOf(resources, resources.length + 1);
         resources[resources.length - 1] = inputStreamResource;
         //字节数组

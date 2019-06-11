81a39e8ed198d78c39babcd3268d42583350fd7e
songwei <820766778@qq.com>
2019-06-12 00:14:51 +0800

    宋伟：简单工厂模式实例。

---
+++ base/src/main/java/com/learning/designmode/simplefactory/Client.java
@@ -0,0 +1,21 @@
+package com.learning.designmode.simplefactory;
+
+/**
+ * @author SONGWEI
+ * @version 1.0
+ * Date: 2019/6/12/012
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        Person person;
+        // 调用简单工厂创建对象
+        person = NvWaFactory.createPerson("M");
+        person.eat();
+
+
+        System.out.println("----------------------");
+        person = NvWaFactory.createPerson("W");
+        person.eat();
+    }
+}
---
+++ base/src/main/java/com/learning/designmode/simplefactory/NvWaFactory.java
@@ -0,0 +1,30 @@
+package com.learning.designmode.simplefactory;
+
+/**
+ * @author SONGWEI
+ * @version 1.0
+ * Date: 2019/6/12/012
+ * 简单工厂模式(不属于GoF23种设计模式)，又名静态工厂模式，适用于固定的几种类型，基本不会添加新的类型的场景。
+ * 当需要添加新的类型时，必须修改工厂类的逻辑。符合单一职责，不符合开闭原则。
+ */
+public class NvWaFactory {
+
+    /**
+     * 模拟女娲造人，根据入参判断需要创建的对象
+     * @return Person
+     */
+    public static Person createPerson(String type) {
+        Person person;
+        switch (type) {
+            case "M":
+                person = new Man();
+                break;
+            case "W":
+                person = new Woman();
+                break;
+            default:
+                throw new RuntimeException("参数非法,你要造什么玩意儿？");
+        }
+        return person;
+    }
+}

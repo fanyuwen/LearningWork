07790d398dceafd96917e6d85b31acd330cdde0a
fanyuwen <18762677070@163.com>
2019-06-14 16:31:02 +0800

    1.拆分Spring 出一个 SpringMVC 2.修改shiro

---
+++ springMVC/src/main/java/com/learning/MVCConfigure.java
@@ -0,0 +1,12 @@
+package com.learning;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yuwen.fan
+ * @date 2019/6/14 16:28
+ */
+@Configuration
+public class MVCConfigure {
+
+}

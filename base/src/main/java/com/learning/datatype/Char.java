a4207713af8f51c745ff6012cd38bd2fba39505b
fanyuwen <18762677070@163.com>
2019-06-14 13:18:25 +0800

    提交了base模块的一些基础知识

--- base/src/main/java/com/learning/datatype/Char.java
+++ base/src/main/java/com/learning/datatype/Char.java
@@ -1,9 +1,5 @@
 package com.learning.datatype;
 
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
 /**
  * @author fanyuwen
  */
@@ -13,15 +9,15 @@ public class Char {
 
     /**
      * java语言本身就是支持Unicode字符编码的,所以编写的java代码里面的每一个字符都支持unicode 详情见 java核心技术卷1
-     *
-     * 下面的unicode描述等价于如下代码
-     * public void fun() {
-     *     System.out.println("java使用unicode编码");
-     * }
+     * <p>
+     * 下面的unicode描述等价于fun方法的定义, 你甚至可以直接将该unicode代码粘贴到外面  效果是一样的
+     * \u0070\u0075\u0062\u006c\u0069\u0063 \u0076\u006f\u0069\u0064 \u0066\u0075\u006e\u0028\u0029 \u007b
+     * \u0053\u0079\u0073\u0074\u0065\u006d\u002e\u006f\u0075\u0074\u002e\u0070\u0072\u0069\u006e\u0074\u006c\u006e\u0028\u0022\u006a\u0061\u0076\u0061\u4f7f\u7528\u0075\u006e\u0069\u0063\u006f\u0064\u0065\u7f16\u7801\u0022\u0029\u003b
+     * \u007d
      */
-    \u0070\u0075\u0062\u006c\u0069\u0063 \u0076\u006f\u0069\u0064 \u0066\u0075\u006e\u0028\u0029 \u007b
-        \u0053\u0079\u0073\u0074\u0065\u006d\u002e\u006f\u0075\u0074\u002e\u0070\u0072\u0069\u006e\u0074\u006c\u006e\u0028\u0022\u006a\u0061\u0076\u0061\u4f7f\u7528\u0075\u006e\u0069\u0063\u006f\u0064\u0065\u7f16\u7801\u0022\u0029\u003b
-    \u007d
+    public void fun() {
+        System.out.println("java使用unicode编码");
+    }
 
 
 }

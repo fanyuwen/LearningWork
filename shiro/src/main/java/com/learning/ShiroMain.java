9b6b17191335312d18a0db434a1bc250243b3221
fanyuwen <18762677070@163.com>
2019-06-14 16:17:50 +0800

    1.拆分Spring 出一个 SpringMVC 2.修改shiro

---
+++ shiro/src/main/java/com/learning/ShiroMain.java
@@ -0,0 +1,10 @@
+package com.learning;
+
+/**
+ * @author fanyuwen
+ * @date  2019/6/14 16:08
+ * shiro主类  @see 跟我学shiro https://jinnianshilongnian.iteye.com/blog/2018398
+ */
+public class ShiroMain {
+
+}

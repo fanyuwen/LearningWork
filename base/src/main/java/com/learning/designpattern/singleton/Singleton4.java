c93d228f2e4ca15e0765d090c1a7732fc2effe4a
fanyuwen <18762677070@163.com>
2019-06-14 14:04:52 +0800

    添加单例模式例子

--- base/src/main/java/com/learning/designpattern/singleton/Singleton4.java
+++ base/src/main/java/com/learning/designpattern/singleton/Singleton4.java
@@ -2,7 +2,7 @@ package com.learning.designpattern.singleton;
 
 /**
  * @author yuwen.fan
- * 最安全的单列模式(防止了反射、拷贝(clone)、序列化等对象的创建)
+ * 最安全的单列模式(防止了反射、拷贝(clone)、序列化等对象的创建以及线程安全)
  */
 public enum Singleton4 {
     SINGLETON_4

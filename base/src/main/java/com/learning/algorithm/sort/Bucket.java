fba03bbf65509923270fdd1e83c4b5626a0493b0
fanyuwen <18762677070@163.com>
2019-06-27 09:05:45 +0800

    新增桶排序实现

---
+++ base/src/main/java/com/learning/algorithm/sort/Bucket.java
@@ -0,0 +1,80 @@
+package com.learning.algorithm.sort;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * @author fanyuwen
+ * @date 2019/6/27 8:22
+ * 桶排序实现
+ * 利用数字的每一位在数组中进行排序
+ */
+public class Bucket extends BaseSort {
+
+    private static final int SCALE = 10,
+            FILL_NUM = -1;
+
+    public static void bucketSort(int[] array) {
+        int digit = findMax(array);
+        //桶排序关键(二维数组),第二维是根据每一位十进制数字去排序的
+        int[][] sortArray = initSortArray(array.length, null);
+        while (digit-- > 0) {
+            //从最高位开始获取每一位的数字
+            int high_digit = IntStream.range(1, digit).map(i -> SCALE).reduce(1, (left, right) -> left * right);
+            //遍历数组获取每个元素的最高位
+            for (int i = 0; i < array.length; i++) {
+                int ele = array[i];
+                int curr_digit = ele / high_digit;
+                sortArray[i][curr_digit] = ele;
+            }
+            //反向遍历,从桶的第二维数字开始遍历
+            int index = 0;
+            for (int i = 0; i < SCALE; i++) {
+                for (int j = 0; j < sortArray.length; j++) {
+                    if (sortArray[j][i] > FILL_NUM) {
+                        array[index++] = sortArray[j][i];
+                    }
+                }
+            }
+        }
+    }
+
+    private static int[][] initSortArray(int len, int[][] sortArray) {
+        if (sortArray == null || sortArray.length < len || sortArray[0].length < SCALE) {
+            sortArray = new int[len][SCALE];
+        }
+        for (int[] array : sortArray) {
+            Arrays.fill(array, FILL_NUM);
+        }
+        return sortArray;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(findMax(new int[]{899}));
+        bucketSort(new int[]{899});
+    }
+
+    /**
+     * 从数组中找寻最大的那个数的位数
+     *
+     * @param array 带查找数组
+     * @return 最大数的位数
+     */
+    private static int findMax(int[] array) {
+        int max = 0;
+        //找出数组中最大的数
+        for (int ele : array) {
+            if (ele > max) {
+                max = ele;
+            }
+        }
+        //计算该数拥有的位数
+        int count = 0;
+        while (max != 0) {
+            count++;
+            max /= 10;
+        }
+
+        return count;
+    }
+}

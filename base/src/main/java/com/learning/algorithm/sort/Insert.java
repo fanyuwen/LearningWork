40d3bff7a9ce0bd73304a85d59f32f5ce908f625
fanyuwen <18762677070@163.com>
2019-06-28 00:27:19 +0800

    1.新增插入排序 2.redis应用类添加

---
+++ base/src/main/java/com/learning/algorithm/sort/Insert.java
@@ -0,0 +1,14 @@
+package com.learning.algorithm.sort;
+
+/**
+ * @author fanyuwen
+ * @date 2019/6/27 23:55
+ * 插入排序,代码实现
+ */
+public class Insert {
+
+    public void sort(int[] array) {
+
+    }
+
+}
---
+++ redis/src/main/java/com/learning/redis/Application.java
@@ -0,0 +1,9 @@
+package com.learning.redis;
+
+/**
+ * @author fanyuwen
+ * @date 2019/6/28 0:14
+ * https://my.oschina.net/u/4142269/blog/3063416
+ */
+public class Application {
+}

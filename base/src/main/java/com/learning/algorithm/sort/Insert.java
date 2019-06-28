9543ea45dcdc925cfcbaaff2b864351da17361b2
fanyuwen <18762677070@163.com>
2019-06-28 16:34:46 +0800

    1.新增插入排序实现和单元测试

--- base/src/main/java/com/learning/algorithm/sort/Insert.java
+++ base/src/main/java/com/learning/algorithm/sort/Insert.java
@@ -7,24 +7,20 @@ package com.learning.algorithm.sort;
  */
 public class Insert extends BaseSort {
 
+    /**
+     * 经典插入排序实现
+     *
+     * @param array 待排序数组
+     */
     public void sort(int[] array) {
-//        if (checkArray(array) && array.length > 1) {
-//            for (int i = 1, j = i; i < array.length; j = i++) {
-//                int num = array[i];
-//                while (array[--j] > num) {
-//                    array[j + 1] = array[j];
-//                }
-//            }
-//        }
-
-
-        for (System.out.println("樊宇雯"); true; System.out.println("柏人杰")) {
-
+        if (checkArray(array) && array.length > 1) {
+            for (int i = 1, j = i; i < array.length; j = ++i) {
+                int num = array[i];
+                while (--j > -1 && array[j] > num) {
+                    array[j + 1] = array[j];
+                }
+                array[j + 1] = num;
+            }
         }
     }
-
-    public static void main(String[] args) {
-        new Insert().sort(null);
-    }
-
 }
---
+++ base/src/test/java/com/learning/algorithm/sort/InsertTest.java
@@ -0,0 +1,27 @@
+package com.learning.algorithm.sort;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author fanyuwen
+ * @date 2019/6/28 16:31
+ */
+public class InsertTest extends BaseArrayTest {
+
+    @Test
+    public void testInsert() {
+        Insert insert = new Insert();
+        System.out.println("排序前: " + Arrays.toString(test_arrays));
+        int[] temp_array = test_arrays.clone();
+        insert.sort(test_arrays);
+        Arrays.sort(temp_array);
+        System.out.println("排序后: " + Arrays.toString(test_arrays));
+        System.out.println("排序后: " + Arrays.toString(temp_array));
+
+        Assert.assertArrayEquals(test_arrays, temp_array);
+    }
+
+}

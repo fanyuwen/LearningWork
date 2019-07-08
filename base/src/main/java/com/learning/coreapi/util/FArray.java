d7d2200a7734276e40307d1a2e82124e6c6aebf8
fanyuwen <18762677070@163.com>
2019-07-08 17:00:46 +0800

    数组api的补充

--- base/src/main/java/com/learning/coreapi/util/FArray.java
+++ base/src/main/java/com/learning/coreapi/util/FArray.java
@@ -7,4 +7,13 @@ package com.learning.coreapi.util;
  */
 public class FArray {
 
+    public static final byte[] bytes = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private static final short[] shorts = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    private static final int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static final char[] chars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'};
+    public static final long[] longs = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static final float[] floats = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
+    public static final double[] doubles = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
+    public static final boolean[] booleans = {true, false, true, false, true, false, true, false, true, false};
+
 }

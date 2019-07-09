7451af68812162f666b3e91c5db2fff1f02fe7f3
fanyuwen <18762677070@163.com>
2019-07-10 00:14:08 +0800

    新增类初始化接口常量属性初始化问题

--- base/src/main/java/com/learning/classload/JavaClassLoader.java
+++ base/src/main/java/com/learning/classload/JavaClassLoader.java
@@ -4,6 +4,14 @@ package com.learning.classload;
  * @author fanyuwen
  * @date 2019/6/15 22:36
  * 测试java对象创建初始化顺序
+ *
+ * 类被初始化的时机:
+ * 1.创建类的实例 (new 运算符)
+ * 2.访问类的静态属性,或者对该静态属性进行赋值
+ * 3.调用类的静态方法
+ * 4.使用反射 ({@link Class#forName(String)})
+ * 5.初始化一个类的子类 包括创建
+ * 6.JVM启动时标明的启动类
  */
 public class JavaClassLoader extends FatherLoader {
     static {

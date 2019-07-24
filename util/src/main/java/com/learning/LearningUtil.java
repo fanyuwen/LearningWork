29626cd54e7d9122a04f1b8f19c617f9235c0c93
fanyuwen <18762677070@163.com>
2019-07-25 01:14:57 +0800

    新增技术文档

--- util/src/main/java/com/learning/LearningUtil.java
+++ util/src/main/java/com/learning/LearningUtil.java
@@ -6,6 +6,8 @@ package com.learning;
  * 学习工具类,仅仅只是记录一些关键性的网页知识分享
  * http content-type相关 https://zccst.iteye.com/blog/2180127
  * cros 跨域相关 https://my.oschina.net/incess/blog/3060511
+ * 读写锁——ReentrantReadWriteLock原理详解 https://my.oschina.net/u/3995125/blog/3076888
+ * JDK8中新增的StampedLock https://my.oschina.net/u/3995125/blog/3077082
  */
 public class LearningUtil {
 }
